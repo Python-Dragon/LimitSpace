3e9f41e0e065ab08b382272f0f3822db40c7ea1a
python-dragon <thomas.e.gruenewald@gmail.com>
2023-10-01 10:40:26 -0600

    integration of the items in inventory

--- Inventorylimits/Assets/ItemPickUp.cs
+++ Inventorylimits/Assets/ItemPickUp.cs
@@ -2,35 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicItem : MonoBehaviour
+public class ItemPickUp : MonoBehaviour
 {
-    public int damage;
-    public float distance;
-    public bool consumable;
-    public bool acting;
-    public bool used;
     public GameObject master;
     public ClickMaster masterAct;
-
     // Start is called before the first frame update
     void Start()
     {
         master = GameObject.FindGameObjectWithTag("Master");
         masterAct = master.GetComponent<ClickMaster>();
-        used = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (used)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-    private void OnMouseUp()
-    {
-        masterAct.active = this.gameObject;
-        acting = true;
+        
     }
 }
