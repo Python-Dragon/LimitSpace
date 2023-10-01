9f98d0d38c0cd4beeda9156ada4f38c4bfa96cfa
Terry Gruenewald <tgruenewald@users.noreply.github.com>
2023-10-01 17:21:12 -0600

    fix inventory prefabs (#12)

--- Inventorylimits/Assets/BasicItem.cs
+++ Inventorylimits/Assets/BasicItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -30,6 +31,7 @@ public class BasicItem : MonoBehaviour
     }
     private void OnMouseUp()
     {
+        Console.WriteLine("picking up item");
         masterAct.active = this.gameObject;
         acting = true;
     }
